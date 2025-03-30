d51aa1fed1c1256b6078062822fe80d473117e20
Ademola Onasoga <ademola.onasoga@gmail.com>
2025-03-30 21:25:15 +0100

    config: add analytics

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,11 @@ import { Provider } from "react-redux";
 import { persistor, store } from "./store/store.ts";
 import { PersistGate } from "redux-persist/integration/react";
 import { Toaster } from "react-hot-toast";
+import Clarity from "@microsoft/clarity";
+
+const projectId = "qwh132s37s";
+
+Clarity.init(projectId);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
